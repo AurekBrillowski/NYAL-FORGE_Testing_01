082cee45ed9bfe897af1488894a5b7e128bea5a0
AurekBrillowski <aurek@parkcitiesxc.org>
2019-01-27 17:39:18 -0600

    Display 'FORGE: NYAL - Testing' on Home Page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    FORGE: NYAL - Testing
                 </div>
 
                 <div class="links">
