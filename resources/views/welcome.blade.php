b722860b89ccd99e397355db97c6d903340f665c
AurekBrillowski <aurek@parkcitiesxc.org>
2019-01-27 17:47:48 -0600

    Background -> More Green

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -12,7 +12,7 @@
         <!-- Styles -->
         <style>
             html, body {
-                background-color: #eff;
+                background-color: #efe;
                 color: #636b6f;
                 font-family: 'Nunito', sans-serif;
                 font-weight: 200;
