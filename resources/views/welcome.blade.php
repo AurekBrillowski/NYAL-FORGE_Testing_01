aa4ade928b4d5aaea67e0b6b3a28ca781420e9c5
AurekBrillowski <aurek@parkcitiesxc.org>
2019-01-27 18:08:05 -0600

    Background color change

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -12,7 +12,7 @@
         <!-- Styles -->
         <style>
             html, body {
-                background-color: #efe;
+                background-color: #ffe;
                 color: #636b6f;
                 font-family: 'Nunito', sans-serif;
                 font-weight: 200;
